9619c5176b34d889b362bd7600989a9e0a8e80d8
Alvin-45 <webshieldhelp@gmail.com>
2024-02-10 17:05:11 +0530

    file updated2

--- src/App.jsx
+++ src/App.jsx
@@ -21,13 +21,13 @@ function App() {
       <h1 className='pb-5 fw-bolder'>Background Changer</h1>
       <em className="text-secondary pb-3">Select the background colour</em>
       <div className='d-flex justify-content-center align-items-center flex-wrap'>
-        <button className="btn btn-light me-3 text-danger fw-bolder" style={{ width: '120px' }} onClick={() => bgChange('red')} type="button">Red</button>
-        <button className="btn btn-light me-3 text-success fw-bolder" style={{ width: '120px' }} onClick={() => bgChange("green")}>Green</button>
-        <button className="btn btn-light me-3 text-secondary fw-bolder" style={{ width: '120px' }} onClick={() => bgChange("grey")}>Grey</button>
-        <button className="btn btn-light me-3 fw-bolder" style={{ width: '120px' }} onClick={() => bgChange("black")}>Black</button>
-        <button className="btn btn-light me-3 text-warning fw-bolder" style={{ width: '120px' }} onClick={() => bgChange("yellow")}>Yellow</button>
-        <button className="btn btn-light me-3 text-primary fw-bolder" style={{ width: '120px' }} onClick={() => bgChange("blue")}>Blue</button>
-        <button className="btn btn-light me-3 fw-bolder" style={{ width: '120px'}} onClick={() => bgChange("white")}>Reset</button>
+        <button className="btn btn-light me-3 text-danger fw-bolder mb-3" style={{ width: '120px' }} onClick={() => bgChange('red')} type="button">Red</button>
+        <button className="btn btn-light me-3 text-success fw-bolder mb-3" style={{ width: '120px' }} onClick={() => bgChange("green")}>Green</button>
+        <button className="btn btn-light me-3 text-secondary fw-bolder mb-3" style={{ width: '120px' }} onClick={() => bgChange("grey")}>Grey</button>
+        <button className="btn btn-light me-3 fw-bolder mb-3" style={{ width: '120px' }} onClick={() => bgChange("black")}>Black</button>
+        <button className="btn btn-light me-3 text-warning fw-bolder mb-3" style={{ width: '120px' }} onClick={() => bgChange("yellow")}>Yellow</button>
+        <button className="btn btn-light me-3 text-primary fw-bolder mb-3" style={{ width: '120px' }} onClick={() => bgChange("blue")}>Blue</button>
+        <button className="btn btn-light me-3 fw-bolder mb-3" style={{ width: '120px'}} onClick={() => bgChange("white")}>Reset</button>
       </div>
     </div>
   )
